51b2be72fc99707cf6d71e690f04eb55dc35cab1
Priyank Ahuja <priyankahuja02@gmail.com>
2020-04-05 16:40:31 +0530

    Project creation & IBOutlets

---
+++ coder-swag-app/coder-swag-app/View/CategoryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryTableViewCell.swift
+//  coder-swag-app
+//
+//  Created by Pravir Ahuja on 05/04/20.
+//  Copyright © 2020 Priyank Ahuja. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
